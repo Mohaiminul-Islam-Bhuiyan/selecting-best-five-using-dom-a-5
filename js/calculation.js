49b0224cf05f65676420f93fed5b8eeb9aaa18aa
Mohaiminul-Islam-Bhuiyan <rafe.aust@gmail.com>
2022-08-23 11:51:00 +0600

    initially completed

--- js/calculation.js
+++ js/calculation.js
@@ -4,10 +4,18 @@ document.getElementById('btn-calculate').addEventListener('click',function(){
     const perPlayerCost = getInputValueById('per-player-cost');
     console.log(perPlayerCost)
     const totalPlayers = selectedPlayersArray.length;
+
+    if (Math.sign(perPlayerCost) === -1){
+        alert('Enter a valid number')
+        return;
+    }
+
+    else{
     const playerExpense = perPlayerCost * totalPlayers;
     const playerExpenseInitial = getTextElementValueById('player-expense');
     
     setTextElementValueById('player-expense', playerExpense);
+}
 })
 
 // calculating total expense 
@@ -17,9 +25,16 @@ document.getElementById('btn-calculate-total').addEventListener('click', functio
     const coachCost = getInputValueById('coach-cost');
     const playerExpenseValue = getTextElementValueById('player-expense')
     console.log(managerCost, coachCost)
-
+    if (Math.sign(managerCost) === -1 || Math.sign(coachCost) === -1){
+        alert('Enter a valid number')
+        return;
+    }
+    else{
     const total = playerExpenseValue + managerCost + coachCost;
+
+
     const initialTotal = getTextElementValueById('total');
 
     setTextElementValueById('total', total);
+}
 })
