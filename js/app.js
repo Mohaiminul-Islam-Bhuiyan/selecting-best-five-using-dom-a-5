f03b80ba1ca4e595ddc28690943f24680d6f20cc
Mohaiminul-Islam-Bhuiyan <rafe.aust@gmail.com>
2022-08-23 09:48:51 +0600

    player selection added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,42 @@
-console.log('hi')
+const selectedPlayersArray =[];
+
+function displaySelection(allPlayers){
+    const tableBody = document.getElementById('display-selected-players');
+
+    tableBody.innerHTML = '';
+    for (let i = 0; i < allPlayers.length; i++){
+        const name = selectedPlayersArray[i];
+
+        const tr = document.createElement('tr');
+        
+        tr.innerHTML = 
+        `
+        <th scope="row">${i+1}</th>
+        <td>${name}</td>
+        `
+
+        tableBody.appendChild(tr);
+        
+   
+}
+}
+
+
+function selectPlayer(element){
+    // console.log(element.parentNode.children[0].innerText)
+    const playerName = element.parentNode.children[0].innerText;
+    // console.log(playerName)
+
+    if (selectedPlayersArray.length > 4){
+        alert('only 5 players can be added')
+    }
+    else{
+        selectedPlayersArray.push(playerName);
+    }
+    // selectedPlayersArray.push(playerName);
+    // console.log(selectedPlayers)
+
+    element.disabled = true;
+
+    displaySelection(selectedPlayersArray)
+}
